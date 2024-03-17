68804d4fe4b6222658921851521a55a005625d87
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-03-17 19:07:39 +0300

    Revert "Feat/rss pz 10 enable  reversible interaction with word cards (#54)"
    This reverts commit 42f6c2eaee0dcbbe8145988c8f548ec19f65d0f4.

--- index.ts
+++
@@ -1,4 +0,0 @@
-import './style.css';
-import { startApp } from '@/app';
-
-startApp();
--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from './component';
-import { startTraining } from './training';
-
-const app = new Component({ className: 'app' });
-document.body.appendChild(app.getNode());
-
-export function startApp(): void {
-  startTraining();
-}
